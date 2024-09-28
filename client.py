a7d4bfb59e1e59c89d1093304d6134bc00bb32ce
whimsicalwisp <emferra01@gmail.com>
2024-09-28 12:58:52 -0400

    Updated client

--- client.py
+++ client.py
@@ -4,18 +4,19 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Use the server's IP address instead of 'localhost'
-server_address = ('ip-address', 123)  # Replace with the actual IP address of the server
+server_address = ('192.168.77.246',12345)  # Replace with the actual IP address of the server
 client_socket.connect(server_address)
 
 try:
-    # Send data
-    message = 'hi'.encode()
-    print('Sending:', message.decode())
-    client_socket.sendall(message)
+    while True:
+        # Send data
+        message = input("").encode()
+        print('Sending:', message.decode())
+        client_socket.sendall(message)
 
-    # Wait for a response
-    data = client_socket.recv(1024)
-    print('Received:', data.decode())
+        # Wait for a response
+        data = client_socket.recv(1024)
+        print('Received:', data.decode())
 
 finally:
     client_socket.close()
